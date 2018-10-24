500875d0f693387cbe6febd0460f44c03bce1a8e
gixerr <pwardyn213@gmail.com>
2018-10-24 22:18:19 +0200

    [ADDED] AutoMapper & config

---
+++ src/CookBook.Infrastructure/Mappings/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CookBook.Core.Domain;
+using CookBook.Infrastructure.Dto;
+
+namespace CookBook.Infrastructure.Mappings
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper GetMapper()
+            => new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Recipe, RecipeDto>();
+                cfg.CreateMap<RecipeCategory, RecipeCategoryDto>();
+                cfg.CreateMap<Ingredient, IngredientCategoryDto>();
+                cfg.CreateMap<IngredientCategory, IngredientCategoryDto>();
+            }).CreateMapper();
+    }
+}
