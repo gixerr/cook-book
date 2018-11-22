1764ecafc44b74aab61a4b1b754848ceb3e7fdac
gixerr <pwardyn213@gmail.com>
2018-11-23 00:20:31 +0100

    [INFRASTRUCTURE][MODIFIED] added GetAsync(IngredientCategory ingredientCategory) and code cleanup

--- src/CookBook.Infrastructure/Services/Interfaces/IIngredientService.cs
+++ src/CookBook.Infrastructure/Services/Interfaces/IIngredientService.cs
@@ -1,17 +1,19 @@
+using CookBook.Core.Domain;
+using CookBook.Infrastructure.Dto;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CookBook.Infrastructure.Dto;
 
 namespace CookBook.Infrastructure.Services.Interfaces
 {
     public interface IIngredientService
     {
-         Task<IEnumerable<IngredientDto>> GetAllAsync();
-         Task<IEnumerable<IngredientDto>> GetAsync(string name);
-         Task<IngredientDto> GetAsync(Guid id);
-         Task AddAsync(IngredientCreateDto ingredientDto);
-         Task UpdateAsync(Guid id);
-         Task RemoveAsync(Guid id);
+        Task<IEnumerable<IngredientDto>> GetAllAsync();
+        Task<IEnumerable<IngredientDto>> GetAsync(string name);
+        Task<IEnumerable<IngredientDto>> GetAsync(IngredientCategory ingredientCategory);
+        Task<IngredientDto> GetAsync(Guid id);
+        Task AddAsync(IngredientCreateDto ingredientDto);
+        Task UpdateAsync(Guid id);
+        Task RemoveAsync(Guid id);
     }
 }
