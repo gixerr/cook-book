ae0056bea5adba348cb53c2e071a108c22526966
gixerr <pwardyn213@gmail.com>
2018-11-22 23:38:07 +0100

    [INFASTRUCTURE][ADDED] IngredientUpdateDto

---
+++ src/CookBook.Infrastructure/Dto/IngredientUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CookBook.Infrastructure.Dto
+{
+    public class IngredientUpdateDto
+    {
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
