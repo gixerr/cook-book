cf908618cd1fad04dea189486b941fb207df1952
gixerr <pwardyn213@gmail.com>
2018-11-23 00:27:44 +0100

    [INFRASTRUCTURE][MODIFIED] added error codes and error messages

--- src/CookBook.Infrastructure/Exceptions/ErrorCode.cs
+++ src/CookBook.Infrastructure/Exceptions/ErrorCode.cs
@@ -4,5 +4,7 @@ namespace CookBook.Infrastructure.Exceptions
     {
         public static string NotFound => nameof(NotFound);
         public static string CategoryExists => nameof(CategoryExists);
+        public static string RecipeExists => nameof(RecipeExists);
+        public static string IngredientExists => nameof(IngredientExists);
     }
 }
--- src/CookBook.Infrastructure/Exceptions/ErrorMessage.cs
+++ src/CookBook.Infrastructure/Exceptions/ErrorMessage.cs
@@ -1,13 +1,17 @@
-using System;
-
 namespace CookBook.Infrastructure.Exceptions
 {
     public static class ErrorMessage
     {
         public static string NoRecipeCategories => "No recipe categories availible.";
         public static string NoIngredientCategories => "No ingredient categories availible.";
+        public static string NoRecipes => "No recipes availible.";
+        public static string NoIngredients => "No ingredients availible.";
         public static string CategoryNotFound(string identifier) => $"Categoty '{identifier}' not found.";
         public static string CategoryExists(string identifier) => $"Category '{identifier}' already exists.";
         internal static string IngredientNotFound(string identifier) => $"Ingredient '{identifier}' not found.";
+        internal static string RecipeNotFound(string identifier) => $"Recipe '{identifier}' not found.";
+        internal static string RecipeWithCategoryNotFound(string identifier) => $"Recipe with category '{identifier}' not found.";
+        internal static string RecipeExists(string identifier) => $"Recipe '{identifier}' already exists";
+        internal static string IngredientExists(string identifier) => $"Ingredient '{identifier}' already exists";
     }
 }
