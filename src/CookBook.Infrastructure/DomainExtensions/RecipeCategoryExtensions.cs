4ccb6d12f6ffff244e9a42f3112cd76b6a141320
gixerr <pwardyn213@gmail.com>
2018-11-26 23:49:18 +0100

    [INFRASTRUCTURE][ADDED] RecipeCategoryExtensions

---
+++ src/CookBook.Infrastructure/DomainExtensions/RecipeCategoryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CookBook.Core.Domain;
+using CookBook.Infrastructure.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CookBook.Infrastructure.DomainExtensions
+{
+    internal static class RecipeCategoryExtensions
+    {
+        internal static void ThrowServiceExceptionIfNotExist(this RecipeCategory category, string errorCode, string errorMessage)
+            => _ = category ?? throw new ServiceException(errorCode, errorMessage);
+
+        internal static void ThrowServiceExceptionIfExist(this RecipeCategory category, string errorCode, string errorMessage)
+        {
+            if (!(category is null))
+            {
+                throw new ServiceException(errorCode, errorMessage);
+            }
+        }
+
+        internal static void ThrowServiceExceptionIfNotExist(this IEnumerable<RecipeCategory> categories, string errorCode, string errorMessage)
+        {
+            if (categories?.Any() is false)
+            {
+                throw new ServiceException(errorCode, errorMessage);
+            }
+        }
+    }
+}
