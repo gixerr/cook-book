53c6c1e6a0a7259e1543fe700fa217ef2296d759
gixerr <pwardyn213@gmail.com>
2018-11-26 23:44:06 +0100

    [INFRASTRUCTURE][ADDED] IngredientExtensions

---
+++ src/CookBook.Infrastructure/DomainExtensions/IngredientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using CookBook.Core.Domain;
+using CookBook.Infrastructure.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CookBook.Infrastructure.DomainExtensions
+{
+    internal static class IngredientExtensions
+    {
+        internal static void ThrowServiceExceptionIfNotExist(this Ingredient ingredient, string errorCode, string errorMessage)
+            => _ = ingredient ?? throw new ServiceException(errorCode, errorMessage);
+
+        internal static void ThrowServiceExceptionIfNotExist(this IEnumerable<Ingredient> ingredients, string errorCode, string errorMessage)
+        {
+            if (ingredients?.Any() is false)
+            {
+                throw new ServiceException(errorCode, errorMessage);
+            }
+        }
+
+        internal static void ThrowServiceExceptionIfExist(this IEnumerable<Ingredient> ingredients, string categoryName, string errorCode, string errorMessage)
+        {
+            if (ingredients?.Any(x => x.Category.Name.Equals(categoryName, StringComparison.InvariantCultureIgnoreCase)) is true)
+            {
+                throw new ServiceException(errorCode, errorMessage);
+            }
+        }
+    }
+}
