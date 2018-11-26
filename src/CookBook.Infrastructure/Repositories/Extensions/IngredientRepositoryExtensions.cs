c7ed885071c869ca023d82ce2a10b214b24f6735
gixerr <pwardyn213@gmail.com>
2018-11-26 23:45:18 +0100

    [INFRASTRUCTURE][MODIFIED] removed domain extensions from IngredientRepositoryExtensions

--- src/CookBook.Infrastructure/Repositories/Extensions/IngredientRepositoryExtensions.cs
+++ src/CookBook.Infrastructure/Repositories/Extensions/IngredientRepositoryExtensions.cs
@@ -1,10 +1,10 @@
 using CookBook.Core.Domain;
 using CookBook.Core.Repositories;
+using CookBook.Infrastructure.DomainExtensions;
 using CookBook.Infrastructure.Dto;
 using CookBook.Infrastructure.Exceptions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CookBook.Infrastructure.Repositories.Extensions
@@ -72,25 +72,6 @@ namespace CookBook.Infrastructure.Repositories.Extensions
             var ingredient = await repository.GetOrThrowAsync(id);
             ingredient.ThrowServiceExceptionIfNotExist(ErrorCode.NotFound, ErrorMessage.RecipeNotFound(id.ToString()));
             await repository.RemoveAsync(id);
-        }
-
-        internal static void ThrowServiceExceptionIfNotExist(this Ingredient ingredient, string errorCode, string errorMessage)
-            => _ = ingredient ?? throw new ServiceException(errorCode, errorMessage);
-
-        internal static void ThrowServiceExceptionIfNotExist(this IEnumerable<Ingredient> ingredients, string errorCode, string errorMessage)
-        {
-            if (ingredients?.Any() is false)
-            {
-                throw new ServiceException(errorCode, errorMessage);
-            }
-        }
-
-        internal static void ThrowServiceExceptionIfExist(this IEnumerable<Ingredient> ingredients, string categoryName, string errorCode, string errorMessage)
-        {
-            if (ingredients?.Any(x => x.Category.Name.Equals(categoryName, StringComparison.InvariantCultureIgnoreCase)) is true)
-            {
-                throw new ServiceException(errorCode, errorMessage);
-            }
         }
     }
 }
