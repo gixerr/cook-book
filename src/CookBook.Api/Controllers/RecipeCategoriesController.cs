452d53dd65fe05bec1fe5f7574b72ba8cf862342
gixerr <pwardyn213@gmail.com>
2018-12-04 23:07:11 +0100

    [API][ADDED] RecipeCategoriesController

---
+++ src/CookBook.Api/Controllers/RecipeCategoriesController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Threading.Tasks;
+using CookBook.Infrastructure.Commands;
+using CookBook.Infrastructure.Commands.Interfaces;
+using CookBook.Infrastructure.Dto;
+using CookBook.Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookBook.Api.Controllers
+{
+    public class RecipeCategoriesController : BaseController
+    {
+        private readonly IRecipeCategoryService _categoryService;
+
+        public RecipeCategoriesController(ICommandDispatcher commandDispatcher, IRecipeCategoryService categoryService)
+            : base(commandDispatcher)
+        {
+            _categoryService = categoryService;    
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            var category = _categoryService.GetAsync(id);
+
+            return Ok(category);
+        }
+
+        [HttpGet("name/{name}")]
+        public async Task<ActionResult> Get(string name)
+        {
+            var category = await _categoryService.GetAsync(name);
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(RecipeCategoryCreateDto command)
+        {
+            await CommandDispatcher.DispatchAsync(command);
+
+            return Created($"api/recipecategories/{command.Name}", null);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(RecipeCategoryUpdateDto command)
+        {
+            await CommandDispatcher.DispatchAsync(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(RecipeCategoryRemoveDto command)
+        {
+            await CommandDispatcher.DispatchAsync(command);
+
+            return NoContent();
+        }
+        
+    }
+}
