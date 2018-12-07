e6debf1c46fb6af032101434e7058fae169b04b8
gixerr <pwardyn213@gmail.com>
2018-12-07 21:07:35 +0100

    [INFRASTRUCTURE][ADDED] added Id property to RecipeUpdateDto

--- src/CookBook.Infrastructure/Dto/RecipeUpdateDto.cs
+++ src/CookBook.Infrastructure/Dto/RecipeUpdateDto.cs
@@ -1,9 +1,11 @@
+using System;
 using CookBook.Infrastructure.Commands.Interfaces;
 
 namespace CookBook.Infrastructure.Dto
 {
     public class RecipeUpdateDto : ICommand
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string CategoryName { get; set; }
         public string ShortDescription { get; set; }
