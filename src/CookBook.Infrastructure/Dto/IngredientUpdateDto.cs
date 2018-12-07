253a9bdf855efcba3c2ababb3999efc087c019da
gixerr <pwardyn213@gmail.com>
2018-12-07 17:54:01 +0100

    [INFRASTRUCTURE][MODIFIED] added Id propperty to IngredientUpdateDto

--- src/CookBook.Infrastructure/Dto/IngredientUpdateDto.cs
+++ src/CookBook.Infrastructure/Dto/IngredientUpdateDto.cs
@@ -1,9 +1,11 @@
-﻿using CookBook.Infrastructure.Commands.Interfaces;
+﻿using System;
+using CookBook.Infrastructure.Commands.Interfaces;
 
 namespace CookBook.Infrastructure.Dto
 {
     public class IngredientUpdateDto : ICommand
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string CategoryName { get; set; }
     }
