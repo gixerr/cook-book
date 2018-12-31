c5652f9d04b1b75f558be3b2748bbfe3f38e93a2
gixerr <pwardyn213@gmail.com>
2018-12-31 14:12:11 +0100

    [INFRASTRUCTURE][MODIFIED]error codes and messages

--- src/CookBook.Infrastructure/Exceptions/ErrorCode.cs
+++ src/CookBook.Infrastructure/Exceptions/ErrorCode.cs
@@ -6,5 +6,6 @@ namespace CookBook.Infrastructure.Exceptions
         public static string CategoryExists => nameof(CategoryExists);
         public static string RecipeExists => nameof(RecipeExists);
         public static string IngredientExists => nameof(IngredientExists);
+        public static string InvalidDataProvider => nameof(InvalidDataProvider);
     }
 }
