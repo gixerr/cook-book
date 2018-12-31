ff99b6852d445055fc4001531de9d67390c68c0d
gixerr <pwardyn213@gmail.com>
2018-12-31 13:58:52 +0100

    [API][MODIFIED] added registation methods
    added registration methods to ContainerBuilderExtensions and removed
    unnecessary method

--- src/CookBook.Api/Extensions/ContainerBuilderExtensions.cs
+++ src/CookBook.Api/Extensions/ContainerBuilderExtensions.cs
@@ -1,20 +1,50 @@
+using System;
 using Autofac;
+using CookBook.Api.Settings;
 using CookBook.Infrastructure.CommandHandlers.Interfaces;
+using CookBook.Infrastructure.Exceptions;
+using CookBook.Infrastructure.IoC.Modules;
 using CookBook.Infrastructure.Mappings;
-using CookBook.Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace CookBook.Api.Extensions
 {
     public static class ContainerBuilderExtensions
     {
-        public static ContainerBuilder RegisterAppTypes(this ContainerBuilder builder)
+        //TODO: Refactor this method
+        public static ContainerBuilder RegisterDataProviderModule(this ContainerBuilder builder,
+            IConfiguration configuration)
         {
-            var infrastructureAssembly = typeof(IRecipeService).Assembly;
-            builder.RegisterAssemblyTypes(infrastructureAssembly)
-                   .AsImplementedInterfaces();
+            var persistence = new PersistenceSettings();
+            configuration.GetSection("persistence").Bind(persistence);
+            if (persistence.ProviderType.Equals("inMemory", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.RegisterModule<InMemoryRepositoryModule>();
+
+                return builder;
+            }
+            if (persistence.ProviderType.Equals("sql", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.RegisterModule<EntityFrameworkRepositoryModule>();
+
+                return builder;
+            }
+            throw new InfrastructureException(ErrorCode.InvalidDataProvider,
+                ErrorMessage.InvalidDataProvider(persistence.ProviderType));
+        }
+
+        public static ContainerBuilder RegisterCommandModule(this ContainerBuilder builder)
+        {
+            var infrastructureAssembly = typeof(CommandModule).Assembly;
             builder.RegisterAssemblyTypes(infrastructureAssembly)
                    .AsClosedTypesOf(typeof(ICommandHandler<>))
                    .InstancePerLifetimeScope();
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterAutoMapper(this ContainerBuilder builder)
+        {
             builder.RegisterInstance(AutoMapperConfig.GetMapper())
                    .SingleInstance();
 
